0be9a77382d4682322d5727f18aa4b7beb1445c7
Am1d1v <dimadiman1adima@gmail.com>
2024-04-11 13:34:03 +0300

    Regular Expressions. Test & Classes

--- RegularExpressions.js
+++ RegularExpressions.js
@@ -11,12 +11,16 @@
 // g
 // m
 
+
+
+
 // Search
 /* const ans = prompt('Type your name');
 
 const reg = /n/i;
 //const reg = /n/ig;
 console.log(ans.search(reg));  */
+
 
 
 // Match 
@@ -26,7 +30,30 @@ const reg = /n/ig;
 console.log(ans.match(reg)); */
 
 
+
 // Replace
-const pass = prompt('Password');
+/* const pass = prompt('Password');
 console.log(pass.replace(/./g, "*"));
-console.log(pass.replace(/\./g, "*"));
+console.log(pass.replace(/\./g, "*")); */
+//console.log('12-34-56'.replace(/-/g, ""));
+//console.log('12-34-56'.replace(/-/g, ":"));
+
+
+
+// Test
+/* const reg = /n/ig;
+console.log(reg.test('Ann'));
+console.log(reg.test('ANN'));
+console.log(reg.test('A')); */
+
+
+// Classes
+// \d - digits
+// \w - words/letters
+// \s - spaces
+
+const reg = /\d/;
+// console.log(reg.test('123'));
+// console.log(reg.test(123));
+// console.log(reg.test('abc'));
+// console.log(reg.test('abc123'));
