258585c5aa129fc5be481d13e1734414784593e3
Am1d1v <dimadiman1adima@gmail.com>
2024-04-30 14:05:55 +0300

    Regular Expressions

--- Example2/RegularExpressions.js
+++ Example2/RegularExpressions.js
@@ -19,8 +19,28 @@ console.log(str.replace(/stRing/i, '$& New'));
 
 
 // Add brackets for tel
-let str = document.querySelector('a').textContent;
+/* let str = document.querySelector('a').textContent;
 let reg = /\d/g;
 let number = str.match(reg).join('');
 document.querySelector('a').setAttribute('href', `tel:+${number}`);
-console.log(number);
+ */
+
+
+
+// Show all digits
+const digits = 'lorem 1234567890 lorem lorem';
+console.log(digits.match(/\d/g));
+
+// Show all spaces
+const spaces = 'lorem 1234567890 lorem lorem';
+console.log(spaces.match(/\s/g));
+
+// Show all words
+const words = 'lorem 1234567890 lorem lorem _ _ lorem';
+console.log(words.match(/\w/g));
+
+// Show all  but digits
+const Digits = 'lorem 1234567890 lorem lorem _ _ lorem';
+console.log(Digits.match(/\D/g));
+
+
