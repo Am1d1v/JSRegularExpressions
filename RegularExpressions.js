48262c877c9d8736b63194b04027787b6665dbad
Am1d1v <dimadiman1adima@gmail.com>
2024-04-30 13:45:01 +0300

    Regular Expressions. Add brackets for tel

--- RegularExpressions.js
+++ RegularExpressions.js
@@ -1,5 +1,26 @@
 
 
+// Match
+/* let string = 'String, one more string';
+console.log(string.match(/string/ig)); */
 
-let string = 'String, one more string';
-console.log(string.match(/string/ig));
+
+// Replace
+/* let str = 'String, one more string, another stRing';
+console.log(str.replace(/stRing/i, 'somethingnew'));
+console.log(str.replace(/stRing/i, '$& New'));
+ */
+
+
+// Test
+// let str = 'String, one more string, another stRing';
+// console.log(/string/gi.test(str));
+
+
+
+// Add brackets for tel
+let str = document.querySelector('a').textContent;
+let reg = /\d/g;
+let number = str.match(reg).join('');
+document.querySelector('a').setAttribute('href', `tel:+${number}`);
+console.log(number);
